fbaaea4360f1d3a9f824bdc8792ce5fdbba5e383
Ziga <ziga.koracin@agiledrop.com>
2020-05-12 08:56:22 +0200

    Dynamicly adding fields.

--- inc/class-agiledrop-form-pages.php
+++ inc/class-agiledrop-form-pages.php
@@ -5,6 +5,8 @@ if ( !class_exists('Agiledrop_Form_Pages' ) ) {
         public function __construct() {
             add_action( 'admin_init', array( $this, 'settings_init' ) );
 	        add_action( 'admin_menu', array( $this, 'options_page' ) );
+	        //Add default option
+            add_option( 'agiledrop_form_fields_options' );
         }
 
 	    public function options_page() {
@@ -26,13 +28,7 @@ if ( !class_exists('Agiledrop_Form_Pages' ) ) {
 
         public function settings_init() {
 	        register_setting( 'agiledrop_form', 'agiledrop_form_options' );
-
-	        add_settings_section(
-	                'agiledrop_section_mail',
-                    false,
-                false,
-                'agiledrop_form'
-            );
+	        register_setting( 'agiledrop_form_fields', 'agiledrop_form_fields_options', array( $this, 'save_fields' ) );
 
 	        add_settings_section(
 		        'agiledrop_section_settings',
@@ -41,12 +37,19 @@ if ( !class_exists('Agiledrop_Form_Pages' ) ) {
 		        'agiledrop_form'
 	        );
 
+	        add_settings_section(
+	                'agiledrop_section_fields',
+                'test',
+                       false,
+                'agiledrop_form_fields'
+            );
+
 	        add_settings_field(
 	            'agiledrop_field_mail',
                     __( 'Notify admin about new submit', 'agiledrop-domain' ),
                     array( $this, 'field_mail' ),
                     'agiledrop_form',
-                'agiledrop_section_mail',
+                'agiledrop_section_settings',
                 [
                         'label_for'             => 'agiledrop_field_mail',
                         'class'                 => 'agiledrop-row',
@@ -66,6 +69,50 @@ if ( !class_exists('Agiledrop_Form_Pages' ) ) {
 			        'agiledrop_custom_data' => 'custom',
 		        ]
 	        );
+
+	        add_settings_field(
+	                'agiledrop_add_field',
+                        __( 'Select input type', 'agiledrop-domain' ),
+                array( $this, 'add_field' ),
+                'agiledrop_form_fields',
+                'agiledrop_section_fields',
+                [
+                        'label_for'    => 'agiledrop_add_field',
+                        'class'        => 'agiledrop-row',
+                        'agiledrop_field_data' => 'custom'
+                ]
+            );
+        }
+
+        public function add_field( $args ) {
+	        $options = get_option( 'agiledrop_form_fields_options' );
+	        ?>
+            <select id="<?php echo esc_attr( $args['label_for'] ); ?>"
+                    data-custom="<?php echo esc_attr( $args['agiledrop_field_data'] ); ?>"
+                    name="agiledrop_form_fields_options[type]"
+            >
+                <option value="text" <?php echo isset( $options[ $args['label_for'] ] ) ? ( selected( $options[ $args['label_for'] ], 'text', false ) ) : ( '' ); ?>>
+			        <?php esc_html_e( 'Text', 'agiledrop-domain' ); ?>
+                </option>
+                <option value="email" <?php echo isset( $options[ $args['label_for'] ] ) ? ( selected( $options[ $args['label_for'] ], 'email', false ) ) : ( '' ); ?>>
+			        <?php esc_html_e( 'Email', 'agiledrop-domain' ); ?>
+                </option>
+            </select>
+            <label for="agiledrop_form_fields_options[title]">Add title to field</label>
+            <input type="text"
+                   name="agiledrop_form_fields_options[title]"
+                   data-custom="<?php esc_attr( $args['agiledrop_field_data'] );?> required"
+            >
+	        <?php
+        }
+
+        public function save_fields( $args ) {
+            $existing_fields = get_option( 'agiledrop_form_fields_options' );
+            $new_field[] = $args;
+            if ( empty( $existing_fields ) ) {
+                return $new_field;
+            }
+            return array_merge( $existing_fields, $new_field );
         }
 
         public function field_mail( $args ) {
--- templates/agiledrop-form-settings.php
+++ templates/agiledrop-form-settings.php
@@ -9,4 +9,27 @@
 		submit_button( 'Save Settings' );
 		?>
     </form>
+
+    <h1>Add Form Field</h1>
+    <form action="options.php" method="post">
+		<?php
+		settings_fields( 'agiledrop_form_fields' );
+		do_settings_sections( 'agiledrop_form_fields' );
+		submit_button( 'Add Field' );
+		?>
+    </form>
+
+    <h1>Form preview</h1>
+    <?php
+        $fields = get_option( 'agiledrop_form_fields_options' );
+        if ( ! $fields ) {
+            echo "<p>No fields, please create some</p>";
+        }else {
+            foreach ( $fields as $field ) :
+    ?>
+    <form action="#">
+        <label for="<?php echo $field['title']; ?>"><?php echo $field['title']?></label>
+        <input type="<?php echo $field['type'];?>">
+    </form>
+    <?php endforeach; }?>
 </div>
